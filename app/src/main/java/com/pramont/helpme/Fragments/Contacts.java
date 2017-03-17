a229f1e8de4b67a0852c88998494749fcdd43fc6
antoniopradoo <antoniopradoo@gmail.com>
2017-03-16 21:07:46 -0600

    Added the views from contacts, still missing the id's

--- app/src/main/java/com/pramont/helpme/Fragments/Contacts.java
+++ app/src/main/java/com/pramont/helpme/Fragments/Contacts.java
@@ -24,7 +24,9 @@ public class Contacts extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
+
+        return rootView;
     }
 
 }
