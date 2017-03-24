9dd80d07436ed362a0a469d97ac1103b8a93d370
antoniopradoo <antoniopradoo@gmail.com>
2017-03-24 12:11:38 -0600

    added edit text dynamic with the properties for email

--- app/src/main/java/com/pramont/helpme/Fragments/Contacts.java
+++ app/src/main/java/com/pramont/helpme/Fragments/Contacts.java
@@ -6,10 +6,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -20,17 +22,19 @@ import com.pramont.helpme.R;
  * A simple {@link Fragment} subclass.
  */
 @RequiresApi(api = Build.VERSION_CODES.M)
-public class Contacts extends Fragment implements View.OnClickListener{
+public class Contacts extends Fragment implements View.OnClickListener {
     private LinearLayout.LayoutParams mLayoutParams;
-    LinearLayout.LayoutParams params;
-    LinearLayout container_contacts_lly;
-    LinearLayout email_container;
-    private TextView email_tv;
-    private TextView phone_tv;
-    private Button addBtn;
-    private Button rmvBtn;
-    int co = 1;
-
+    LinearLayout.LayoutParams mParamsFields;
+    LinearLayout mContainer_contacts_lly;
+    LinearLayout mEmail_container;
+    LinearLayout mSpace_lly;
+    private TextView mEmail_tv;
+    private TextView mPhone_tv;
+    private EditText mEmail_et;
+    private EditText mPhone_et;
+    private Button mAddBtn;
+    private Button mRmvBtn;
+    private int mId = 0;
 
 
     @Override
@@ -39,41 +43,21 @@ public class Contacts extends Fragment implements View.OnClickListener{
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
 
-        // Params for text levels, margins and layOuts
-        params = new LinearLayout.LayoutParams(500,mLayoutParams.WRAP_CONTENT);
-        params.setMargins(50,0,50,0);
-
-
-
         //To set the main layout container
-        container_contacts_lly = (LinearLayout) rootView.findViewById(R.id.container_contacts);
+        mContainer_contacts_lly = (LinearLayout) rootView.findViewById(R.id.container_contacts);
+
+        //To load the buttons add and remove
+        LinearLayout hidden_buttons_lly = (LinearLayout) rootView.findViewById(R.id.hiddenButtons);
+        View buttonsViews = getLayoutInflater(savedInstanceState).inflate(R.layout.hidden_buttons, mContainer_contacts_lly, false);
+        mAddBtn = (Button) buttonsViews.findViewById(R.id.add_btn);
+        mAddBtn.setOnClickListener(this);
 
         // To set the spaces
-        LinearLayout space_lly = new LinearLayout(getContext());
-        space_lly = getSpaceLayout(space_lly);
+        addSpaces();
 
 
-
-
-
-
-
-
-        LinearLayout hidden_buttons_lly = (LinearLayout) rootView.findViewById(R.id.hiddenButtons);
-
-
-        View buttonsViews = getLayoutInflater(savedInstanceState).inflate(R.layout.hidden_buttons, container_contacts_lly, false);
-        addBtn = (Button) buttonsViews.findViewById(R.id.add_btn);
-        addBtn.setOnClickListener(this);
-
-
-
-        container_contacts_lly.addView(space_lly);
-
-
-
-       // container_contacts_lly.addView(space_lly);
-        container_contacts_lly.addView(buttonsViews);
+        // mContainer_contacts_lly.addView(mSpace_lly);
+        mContainer_contacts_lly.addView(buttonsViews);
         return rootView;
     }
 
@@ -94,44 +78,92 @@ public class Contacts extends Fragment implements View.OnClickListener{
     * To set the email container layout
     * */
 
-    private LinearLayout getEmailContLayout(LinearLayout linearLayout, int count){
+    private LinearLayout getEmailContLayout(LinearLayout linearLayout, int count) {
         linearLayout.setId(count);
         linearLayout.setLayoutParams(
                 new LinearLayout.LayoutParams(
-                        mLayoutParams.MATCH_PARENT,mLayoutParams.WRAP_CONTENT));
-        linearLayout.setBackgroundColor(getResources().getColor(R.color.white,null));
+                        mLayoutParams.MATCH_PARENT, mLayoutParams.WRAP_CONTENT));
+        linearLayout.setBackgroundColor(getResources().getColor(R.color.white, null));
         linearLayout.setOrientation(LinearLayout.HORIZONTAL);
         return linearLayout;
-    }
-
-    /*
-    * To set the text labels from email and phone
-    * */
-    private TextView getTextViewLabels(TextView textView, int text, LinearLayout.LayoutParams params){
-        textView = new TextView(getContext());
-        textView.setLayoutParams(params);
-        textView.setText(text);
-        textView.setTypeface(Typeface.DEFAULT_BOLD);
-        return textView;
     }
 
     @Override
     public void onClick(View view) {
 
-        switch (view.getId()){
+        switch (view.getId())
+        {
             case R.id.add_btn:
-                email_tv = getTextViewLabels(email_tv,R.string.email,params);
-                phone_tv = getTextViewLabels(phone_tv,R.string.phone,params);
-
-                email_container = new LinearLayout(getContext());
-                email_container = getEmailContLayout(email_container,view.getId()+co);
-
-                email_container.addView(email_tv);
-                email_container.addView(phone_tv);
-
-                container_contacts_lly.addView(email_container);
-                co++;
+                loadContactsFields();
                 break;
         }
+    }
+
+    /*
+    * method to load all the fields per contact
+    * */
+    private void loadContactsFields() {
+
+        addSpaces();
+        // Params for text levels, margins and layOuts
+        mParamsFields = new LinearLayout.LayoutParams(180, mLayoutParams.WRAP_CONTENT);
+        mParamsFields.setMargins(50, 0, 50, 0);
+
+        mEmail_tv = getView(mEmail_tv, R.string.email, mParamsFields);
+        mPhone_tv = getView(mPhone_tv, R.string.phone, mParamsFields);
+
+        mEmail_container = new LinearLayout(getContext());
+        mEmail_container = getEmailContLayout(mEmail_container, mId);
+
+        mEmail_container.addView(mEmail_tv);
+       // mEmail_container.addView(mPhone_tv);
+
+
+        // Params for text levels, margins and layOuts
+        mParamsFields = new LinearLayout.LayoutParams(mLayoutParams.WRAP_CONTENT, 200);
+
+        mEmail_et = getView(mEmail_et, R.string.email_hint,mParamsFields);
+        mEmail_container.addView(mEmail_et);
+
+
+
+        mContainer_contacts_lly.addView(mEmail_container);
+
+        addSpaces();
+        mId++;
+    }
+
+    /*
+    * Method to add spaces into the views
+    * */
+
+    private void addSpaces() {
+        // To set the spaces
+        mSpace_lly = new LinearLayout(getContext());
+        mSpace_lly = getSpaceLayout(mSpace_lly);
+        mContainer_contacts_lly.addView(mSpace_lly);
+    }
+
+    /*
+    * Method to set the text labels from email and phone
+    * */
+    private TextView getView(TextView textView, int idString, LinearLayout.LayoutParams params) {
+        textView = new TextView(getContext());
+        textView.setLayoutParams(params);
+        textView.setText(idString);
+        textView.setTypeface(Typeface.DEFAULT_BOLD);
+        return textView;
+    }
+
+    /*
+    *  Method to set the edit text
+    * */
+    private EditText getView(EditText editText, int idHintString,LinearLayout.LayoutParams params){
+        editText = new EditText(getContext());
+        editText.setLayoutParams(params);
+        editText.setHint(idHintString);
+        editText.setBackground(null);
+        editText.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        return  editText;
     }
 }
