326f9f32d7e17bf6ac510524444a63105a31b741
antoniopradoo <antoniopradoo@gmail.com>
2017-03-28 14:23:59 -0600

    Added the alert to switch view and validation for yes and no option

--- app/src/main/java/com/pramont/helpme/Fragments/Settings.java
+++ app/src/main/java/com/pramont/helpme/Fragments/Settings.java
@@ -1,30 +1,63 @@
 package com.pramont.helpme.Fragments;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.pramont.helpme.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Settings extends Fragment {
-
-
-    public Settings() {
-        // Required empty public constructor
-    }
+public class Settings extends Fragment implements CompoundButton.OnCheckedChangeListener {
+    private Switch mSwitchGmail;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        mSwitchGmail = (Switch) rootView.findViewById(R.id.sw_email);
+        mSwitchGmail.setOnCheckedChangeListener(this);
+
+        return rootView;
     }
 
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Context context = getContext();
+        if (isChecked)
+        {
+           AlertDialog alertDialog = new AlertDialog.Builder(context)
+                    .setTitle(R.string.msg_email_notification)
+                    .setMessage(R.string.alert_message_gmail_notification)
+                    .setNegativeButton(context.getString(R.string.alert_opt_not), new DialogInterface.OnClickListener() {
+                        @Override public void onClick(DialogInterface dialogInterface, int i) {
+                            //No actions for now
+                            mSwitchGmail.setChecked(false);
+                        }
+                    })
+                    .setPositiveButton(context.getString(R.string.alert_opt_yes), new DialogInterface.OnClickListener() {
+                        @Override public void onClick(DialogInterface dialogInterface, int i) {
+                        //TODO enable to show the section for gmail account
+                        }
+                    })
+                    .show();
+            alertDialog.setCanceledOnTouchOutside(false);
+        }
+        else
+        {
+
+        }
+    }
 }
