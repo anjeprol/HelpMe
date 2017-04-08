78f06b9bace0e4e28132264a3e732dbae163d572
antoniopradoo <antoniopradoo@gmail.com>
2017-04-08 12:59:19 -0500

    added the boundaries from seekbar as 20 % of minimum required

--- app/src/main/java/com/pramont/helpme/Fragments/Settings.java
+++ app/src/main/java/com/pramont/helpme/Fragments/Settings.java
@@ -14,6 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.pramont.helpme.R;
 
@@ -22,7 +23,7 @@ import com.pramont.helpme.R;
  */
 public class Settings extends Fragment implements CompoundButton.OnCheckedChangeListener, SeekBar.OnSeekBarChangeListener {
     private Switch mSwitchGmail;
-    private LinearLayout mGmail_section_ll ;
+    private LinearLayout mGmail_section_ll;
     private SeekBar mSensibility_SB;
     private TextView mProgress_tv;
 
@@ -34,12 +35,11 @@ public class Settings extends Fragment implements CompoundButton.OnCheckedChange
         View rootView = inflater.inflate(R.layout.fragment_settings, container, false);
 
         mSwitchGmail = (Switch) rootView.findViewById(R.id.sw_email);
-        mGmail_section_ll = (LinearLayout) rootView.findViewById(R.id.gmail_login_ll) ;
+        mGmail_section_ll = (LinearLayout) rootView.findViewById(R.id.gmail_login_ll);
         mSensibility_SB = (SeekBar) rootView.findViewById(R.id.level_sensibility_sb);
         mProgress_tv = (TextView) rootView.findViewById(R.id.level_sensibility_percent);
 
-        mProgress_tv.setText(mProgress_tv.getText().toString()
-                +" "+getString(R.string.symb_percentage));
+        mProgress_tv.setText(mProgress_tv.getText().toString() + " " + getString(R.string.symb_percentage));
 
         mGmail_section_ll.setVisibility(View.GONE);
         mSwitchGmail.setOnCheckedChangeListener(this);
@@ -53,19 +53,21 @@ public class Settings extends Fragment implements CompoundButton.OnCheckedChange
         Context context = getContext();
         if (isChecked)
         {
-           AlertDialog alertDialog = new AlertDialog.Builder(context)
+            AlertDialog alertDialog = new AlertDialog.Builder(context)
                     .setTitle(R.string.msg_email_notification)
                     .setMessage(R.string.alert_message_gmail_notification)
                     .setNegativeButton(context.getString(R.string.alert_opt_not), new DialogInterface.OnClickListener() {
-                        @Override public void onClick(DialogInterface dialogInterface, int i) {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
                             //No actions for now
                             mSwitchGmail.setChecked(false);
 
                         }
                     })
                     .setPositiveButton(context.getString(R.string.alert_opt_yes), new DialogInterface.OnClickListener() {
-                        @Override public void onClick(DialogInterface dialogInterface, int i) {
-                        //TODO enable to show the section for gmail account
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            //TODO enable to show the section for gmail account
                             mGmail_section_ll.setVisibility(View.VISIBLE);
                         }
                     })
@@ -80,7 +82,28 @@ public class Settings extends Fragment implements CompoundButton.OnCheckedChange
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        mProgress_tv.setText(progress+" "+getString(R.string.symb_percentage));
+        Integer min = getResources().getInteger(R.integer.min_required_sensibility_int);
+        StringBuilder minReqStringBuilder = new StringBuilder();
+        StringBuilder progressStringBuilder = new StringBuilder();
+        String symbol = getString(R.string.symb_percentage);
+
+        minReqStringBuilder
+                .append(min)
+                .append(" ")
+                .append(symbol);
+
+        if (progress < min)
+        {
+            progress = min;
+            mProgress_tv.setText(minReqStringBuilder.toString());
+            mSensibility_SB.setProgress(progress);
+        }
+        progressStringBuilder
+                .append(progress)
+                .append(" ")
+                .append(symbol);
+
+        mProgress_tv.setText(progressStringBuilder.toString());
     }
 
     @Override
